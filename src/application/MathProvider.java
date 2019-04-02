3d9ecacc2a4ada7fdde9998a4cc228141e7c9373
Supriya Meshram <supriya_meshram@persistent.co.in>
2019-04-02 18:29:05 +0530

    JUnit Framework

---
+++ src/application/MathProvider.java
@@ -0,0 +1,19 @@
+package application;
+
+public class MathProvider {
+	public MathProvider(){}
+	public int Substract(int firstNumber, int secondNumber)
+	{
+		return(firstNumber-secondNumber);
+	}
+	
+	public int Add(int firstNumber, int secondNumber)
+	{
+		return(firstNumber+secondNumber);
+	}
+	public int Multiply(int firstNumber, int secondNumber)
+	{
+		return(firstNumber * secondNumber);
+	}
+
+}
